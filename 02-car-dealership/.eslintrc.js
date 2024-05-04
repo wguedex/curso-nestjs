1c408c3adb72f6d0ff63688b04935d3ab952a8e1
Wilmer Guedez <w.guedex@gmail.com>
2024-05-04 14:53:28 -0500

    02-car-dealership: creación de módulos

--- 02-car-dealership/.eslintrc.js
+++ 02-car-dealership/.eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
-  rules: {
+  rules: { 
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
--- 02-car-dealership/src/app.module.ts
+++ 02-car-dealership/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common'; 
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  exports: []
 })
 export class AppModule {}
