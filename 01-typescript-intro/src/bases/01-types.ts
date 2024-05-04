319b7446c373be44daa75bb0b9e39a9f37cbbde2
Wilmer Guedez <w.guedex@gmail.com>
2024-05-04 06:59:20 -0500

    01-typescript-intro: tipos y bases sobre módulos

---
+++ 01-typescript-intro/src/bases/01-types.ts
@@ -0,0 +1,7 @@
+
+
+export const name: string = "Wilmer";
+export const age: number = 20;
+export const isValid: boolean = true;
+
+console.log(name);
--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,23 +1,15 @@
+import { name, age, isValid } from './bases/01-types';
 import './style.css'
 import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-
+ 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
+    <h1>Hello ${name} ${age} ${isValid}!!!</h1>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    </a>    
   </div>
 `
 
