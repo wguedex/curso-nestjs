4e24f8132a0682adf1a3800a1aa2ff2daf0d78dd
Wilmer Guedez <w.guedex@gmail.com>
2024-05-04 16:24:20 -0500

    02-car-dealership: desactivar prettier - yarn remove prettier

--- 02-car-dealership/src/app.module.ts
+++ 02-car-dealership/src/app.module.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common'; 
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [CarsModule],
   controllers: [],
   providers: [],
   exports: []
