2e1e903297505a9d69c6ca460de831d18f21d1d6
Wilmer Guedez <w.guedex@gmail.com>
2024-05-04 13:03:00 -0500

    01-typescript-intro: decorador de metodo - @deprecated

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,6 +1,7 @@
  
 // import { charmander } from './bases/04-injection'; 
-import { charmander } from './bases/05-decorators';
+// import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2';
 import './style.css'
 import typescriptLogo from './typescript.svg' 
  
