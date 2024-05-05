2195030b39d8e0c9a53e1d70616cdb85f9be2b73
Wilmer Guedez <w.guedex@gmail.com>
2024-05-04 21:58:57 -0500

    02-car-dealership: actualizar el listado de carros

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,12 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 
 import { v4 as uuid } from 'uuid';
-import { CreateCarDTO, UpdateCarDTO } from './dto/index' 
+import { CreateCarDTO, UpdateCarDTO } from './dto/index';
 
 @Injectable()
 export class CarsService {
-
   private cars: Car[] = [
     {
       id: uuid(),
@@ -23,44 +22,52 @@ export class CarsService {
       brand: 'Jeep',
       model: 'Cherokee',
     },
-  ];  
+  ];
 
   fidnAll() {
     return this.cars;
   }
 
   findOneById(id: string) {
-
-    const car = this.cars.find(car => car.id === id)
+    const car = this.cars.find((car) => car.id === id);
 
     if (!car) {
-        throw new NotFoundException(`Car with ID ${id} not found`);
+      throw new NotFoundException(`Car with ID ${id} not found`);
     }
 
     return car;
-
   }
 
-  create(createCarDTO : CreateCarDTO){
-    const car : Car = {
-      'id' : uuid(),
-      'brand' : createCarDTO.brand, 
-      'model' : createCarDTO.model
+  create(createCarDTO: CreateCarDTO) {
+    const car: Car = {
+      id: uuid(),
+      brand: createCarDTO.brand,
+      model: createCarDTO.model,
+    };
 
+    this.cars.push(car);
+
+    return car;
+  }
+
+  update(id: string, updateCarDTO: UpdateCarDTO) {
+
+    let carDB = this.findOneById(id);
+
+    if (updateCarDTO.id && updateCarDTO.id !== id) {
+      throw new BadRequestException('Car id is not valid inside body');
     }
 
-    this.cars.push(car)
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        // Suponemos que `updateCarDto` contiene los datos actualizados del carro y que
+        // se debe mantener el id original
+        carDB = { ...carDB, ...updateCarDTO, id };
+        return carDB;
+      }
+      return car;
+    });
 
-    return car;
+    return carDB;
   }
-
-  update( id : string , updateCarDTO : UpdateCarDTO){
-
-    let car = this.findOneById(id) 
-
-    if (!car) return; 
-    
-    return car;
-  }
-
 }
