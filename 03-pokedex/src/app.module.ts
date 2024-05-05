43d975dccc07b039b9f3b5abfba5352722646270
Wilmer Guedez <w.guedex@gmail.com>
2024-05-05 15:43:33 -0500

    03-pokedex: conectar nest con mongo

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-    PokemonModule 
+    PokemonModule , 
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon')
   ],
   controllers: [],
   providers: [],
