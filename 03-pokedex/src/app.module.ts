dc8535857dca441497331afb9313a361c7211f2d
Wilmer Guedez <w.guedex@gmail.com>
2024-05-05 13:48:11 -0500

    03-pokedex: servir contenido estático

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'; 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+      }) 
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  
+}
