dd1c56fa5776ce8c68f601f49bec848c484ebb20
Wilmer Guedez <w.guedex@gmail.com>
2024-05-06 23:13:15 -0500

    03-pokedex: validar y eliminar en una sola consulta

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -97,11 +97,10 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    // const pokemon = await this.findOne(id);
-    // await pokemon.deleteOne();
-
-    const result = this.pokemonModel.findByIdAndDelete(id);
-    return result;
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+    }
+    return;
   }
-  
 }
