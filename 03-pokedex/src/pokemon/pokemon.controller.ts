348d32d3f0ee307fa2a293b154d29a0e2ab7ec72
Wilmer Guedez <w.guedex@gmail.com>
2024-05-06 22:56:36 -0500

    03-pokedex: eliminar un pokemon

--- 03-pokedex/src/pokemon/pokemon.controller.ts
+++ 03-pokedex/src/pokemon/pokemon.controller.ts
@@ -29,6 +29,6 @@ export class PokemonController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.pokemonService.remove(+id);
+    return this.pokemonService.remove(id);
   }
 }
--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -97,7 +97,9 @@ export class PokemonService {
     );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
-  }
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
+}
+
 }
