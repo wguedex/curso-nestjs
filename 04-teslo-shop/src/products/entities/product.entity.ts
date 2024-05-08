9b142df81a50f139f8b6cf0458bf2aecb79616b9
Wilmer Guedez <w.guedex@gmail.com>
2024-05-08 18:47:00 -0500

    04-teslo-shop (TypeORM - Postgres): agregar columnas a entidad de productos

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -12,5 +12,35 @@ export class Product {
     })
     title: string;
 
+    @Column('numeric', {
+        default:0
+    })
+    price: number;
+
+    @Column({
+        type:'text', 
+        nullable: true
+    })
+    description: string;
+
+    @Column('text',{
+        unique: true
+    })
+    slug: string;
+
+    @Column('int',{
+        default:0
+    })
+    stock:number;
+
+    @Column('text', {
+        array:true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+
 
 }
