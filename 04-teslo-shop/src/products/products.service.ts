85bba40412e86e64420c45bc5e50954b12c72d30
Wilmer Guedez <w.guedex@gmail.com>
2024-05-09 20:40:24 -0500

    (04-teslo-shop):insertar usando typeORM

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ) {}  
+  
+  async create(createProductDto: CreateProductDto) {
+
+    try {
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save( product );
+
+      return product;
+      
+    } catch (error) {
+     throw new  InternalServerErrorException('Error:',error)
+      // this.handleDBExceptions(error);
+    }
   }
 
   findAll() {
