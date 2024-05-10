d551cdb246af15c9f22e9aa4720a00404dc4ac9f
Wilmer Guedez <w.guedex@gmail.com>
2024-05-10 00:13:17 -0500

    04-teslo-shop (TypeORM - Postgres): manejo de errores

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +7,8 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductService');
   
   constructor(
 
@@ -25,10 +27,22 @@ export class ProductsService {
       return product;
       
     } catch (error) {
-     throw new  InternalServerErrorException('Error:',error)
-      // this.handleDBExceptions(error);
+
+      this.handleDBExceptions(error);
+
     }
   }
+
+  private handleDBExceptions(error: any) {
+ 
+      if (error.code === '23505')
+        throw new BadRequestException(error.detail)
+
+      this.logger.error(error);
+     throw new  InternalServerErrorException('Unexpected error, check server logs')
+    
+  }
+
 
   findAll() {
     return `This action returns all products`;
