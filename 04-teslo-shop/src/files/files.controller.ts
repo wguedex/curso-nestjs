747582d534738c75ce006550e30035e48ee14188
Wilmer Guedez <w.guedex@gmail.com>
2024-05-11 18:48:13 -0500

    04-teslo-shop (Carga de archivos): retornar el secureUrl

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -5,11 +5,14 @@ import { diskStorage } from 'multer';
  
 import {fileFilter, fileNamer} from './helpers/index';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
 
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService, 
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -46,7 +49,8 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image'); 
   }
 
-    const secureUrl = `${ file.filename }`;
+    // const secureUrl = `${ this.configService.get('HOT_API') }/files/product/${file.filename}`;
+    const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${ file.filename }`;
 
     return {secureUrl};
    
--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
-    );
+  );
 
-  await app.listen(3004);
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${ process.env.PORT }`);
 }
 bootstrap();
