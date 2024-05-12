38c67889c112c88ed1764b1705f1bccd6a1e7d8b
Wilmer Guedez <w.guedex@gmail.com>
2024-05-12 15:47:01 -0500

    04-teslo-shop (autenticación y autorización): Nest Authentication - Passport

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -3,12 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),  
+    PassportModule.register({defaultStrategy:'jwt'}), 
+    // JwtModule.register({
+    //   secret:process.env.HOST_API,
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
   ], 
   // exports: [TypeOrmModule]
 })
