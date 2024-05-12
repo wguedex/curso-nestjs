de032f39cd64d26093e67a337026d1553567408d
Wilmer Guedez <w.guedex@gmail.com>
2024-05-11 22:27:01 -0500

    04-teslo-shop (Carga de archivos): otras formas de desplegar archivos

--- 04-teslo-shop/src/products/products.module.ts
+++ 04-teslo-shop/src/products/products.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product, ProductImage } from './entities'; 
+import { Product, ProductImage } from './entities';   
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [
-    TypeOrmModule.forFeature([Product, ProductImage])
+    TypeOrmModule.forFeature([Product, ProductImage]),  
   ], 
   exports: [ProductsService]
 })
