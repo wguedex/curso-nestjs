28a33e54af6195151b450f2802e5be59d9e0b880
Wilmer Guedez <w.guedex@gmail.com>
2024-05-15 00:31:58 -0500

    04-teslo-shop (autenticación y autorización): verificar rol del usuario

--- 04-teslo-shop/src/auth/guards/user-role/user-role.guard.ts
+++ 04-teslo-shop/src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -16,7 +17,37 @@ export class UserRoleGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
 
     const validRoles: string[] = this.reflector.get('roles', context.getHandler())
+
+
+    if (!validRoles) {
+      return true
+    }
+
+    if (validRoles.length === 0) {
+      return true
+    }
+
     console.log({validRoles});
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+    
+    if (!user) {
+        throw new BadRequestException('User not found');
+    }
+    
+    // console.log({ userRoles: user.roles });
+
+    for (const role of user.roles) {
+      if ( validRoles.includes(role) ) {
+        return true;
+      }
+    }
+    
+    throw new ForbiddenException(
+      `User ${ user.fullname } need a valid role: [${validRoles}]`
+    );
+    
 
     // console.log('user role guard');
     return true;
