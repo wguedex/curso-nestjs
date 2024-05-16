522c44f8b14f4689c682ab79f820778dca987583
Wilmer Guedez <w.guedex@gmail.com>
2024-05-15 22:36:26 -0500

    04-teslo-shop (autenticación y autorización): auth en otros modulos

--- 04-teslo-shop/src/products/products.controller.ts
+++ 04-teslo-shop/src/products/products.controller.ts
@@ -3,12 +3,16 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('products')
+
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.user)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -25,11 +29,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.user)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.user)
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
--- 04-teslo-shop/src/seed/seed.controller.ts
+++ 04-teslo-shop/src/seed/seed.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service'; 
+import { Auth } from '../auth/decorators'; 
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
  
   @Get()
+  @Auth(ValidRoles.admin)
   executeSeed() {
     return this.seedService.runSeed();
   }
