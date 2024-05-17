8a16ec022eab2d2695b7f1d72de077c080ad2f06
Wilmer Guedez <w.guedex@gmail.com>
2024-05-17 01:19:35 -0500

    04-teslo-shop (websockets): server - escuchar conexiones y desconexiones

--- 04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
+++ 04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true})
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleDisconnect(client: Socket) {
+    console.log('cliente desconectado', client.id)
+    // throw new Error('Method not implemented.');
+  }
+
+  handleConnection(client: Socket) {
+    console.log('cliente conectado', client.id)
+    // throw new Error('Method not implemented.');
+  }
+
 }
