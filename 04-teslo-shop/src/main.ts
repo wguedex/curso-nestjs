8de1f3294d9107231d72243163b57c4202722067
Wilmer Guedez <w.guedex@gmail.com>
2024-05-16 21:18:09 -0500

    04-teslo-shop (autenticación y autorización): NestJs swagger - OpenAPI Specification

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
     })
   );
 
+
+  const config = new DocumentBuilder()
+    .setTitle('04-teslo-shop')
+    .setDescription('Teslo shop endpoints')
+    .setVersion('1.0') 
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${ process.env.PORT }`);
