4050de260c2503c3de7ade97e72cff62ecd7ba87
Wilmer Guedez <w.guedex@gmail.com>
2024-05-18 03:53:11 -0500

    05-websocket-client (websockets): cliente - detectar conexion y desconexion

--- 05-websocket-client/src/main.ts
+++ 05-websocket-client/src/main.ts
@@ -4,7 +4,7 @@ import { connectToServer } from './socket-client.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div>
   <h1>Websocket - Client</h1>
-  <span>Offline</span>
+  <span id="server-status">Offline</span>
 </div>
 `
 connectToServer()
--- 05-websocket-client/src/socket-client.ts
+++ 05-websocket-client/src/socket-client.ts
@@ -1,4 +1,4 @@
-import { Manager } from "socket.io-client"
+import { Manager, Socket } from "socket.io-client"
 
 
 
@@ -9,8 +9,26 @@ export const connectToServer = () => {
     const manager = new Manager('localhost:3000/socket.io/socket.io.js');
 
     const socket = manager.socket('/');
+    addListeners(socket)
     // console.log({socket})
 
 
+
+}
+
+
+const addListeners = (socket: Socket) => {
+    
+    const serverStatusLabel = document.querySelector('#server-status')!;
+    
+    socket.on('connect', () => {
+        // console.log('connected')
+        serverStatusLabel.innerHTML = 'connected';
+    });
+
+    socket.on('disconnect', () => {
+        // console.log('disconnect')
+        serverStatusLabel.innerHTML = 'disconnect';
+    });    
 
 }
